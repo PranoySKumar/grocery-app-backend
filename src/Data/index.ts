dd4bb801ebff8c7a67a4f41e856a1f2bb165a348
PranoySKumar <pranoyskumar@gmail.com>
2022-10-30 21:24:31 +0530

    added export in index file for the renamed enum

--- src/Data/index.ts
+++ src/Data/index.ts
@@ -1,3 +1,3 @@
 export { Discount, Quantity } from "./product-enum";
-export { OrderStatus } from "./order-status-enum";
-export { Notification } from "./notification-enum";
+export { OrderStatus } from "./orders-enum";
+export { NotificationType } from "./notification-enum";
