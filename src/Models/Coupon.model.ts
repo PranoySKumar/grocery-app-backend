7f08910455e658257660d3274f9bf0e189588691
PranoySKumar <pranoyskumar@gmail.com>
2022-10-31 15:33:47 +0530

    added coupon model

---
+++ src/Models/Coupon.model.ts
@@ -0,0 +1,34 @@
+import { model, ObjectId, Schema, SchemaTypes } from "mongoose";
+
+type CouponDiscount = {
+  upto: number;
+  percentage: number;
+};
+
+export interface ICoupon {
+  _id?: ObjectId;
+  title: string;
+  description: string;
+  couponDiscount: CouponDiscount;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const couponDiscountSchema = new Schema<CouponDiscount>(
+  {
+    upto: { type: Number, required: true },
+    percentage: { type: Number, required: true },
+  },
+  { _id: false }
+);
+
+const couponSchema = new Schema<ICoupon>(
+  {
+    title: { type: SchemaTypes.String, required: true },
+    description: { type: SchemaTypes.String, required: true },
+    couponDiscount: { type: couponDiscountSchema, required: true },
+  },
+  { timestamps: true }
+);
+
+export const Coupon = model("Coupons", couponSchema);
--- src/Models/index.ts
+++ src/Models/index.ts
@@ -4,3 +4,4 @@ export { Store, IStore } from "./Store.model";
 export { Category, ICategory } from "./Category.model";
 export { Order, IOrder } from "./Order.model";
 export { Notification, INotification } from "./Notification.model";
+export { ICoupon, Coupon } from "./Coupon.model";
