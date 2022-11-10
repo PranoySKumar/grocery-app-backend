4b657903ae5d9974fd812af74ca5fac8663551b8
PranoySKumar <pranoyskumar@gmail.com>
2022-11-10 11:57:49 +0530

    updated delete method

--- src/Services/category-services.ts
+++ src/Services/category-services.ts
@@ -1,11 +1,11 @@
 import { Category, ICategory } from "../Models";
-
+import { Types } from "mongoose";
 export default class CategoryService {
   static async add(name: string, type: string) {
     return await new Category({ name, type }).save();
   }
   static async delete(_id: string) {
-    return await Category.findByIdAndDelete(_id);
+    return await Category.deleteOne({ _id: new Types.ObjectId(_id) });
   }
   static async update(_id: string, data: { type?: string; name?: string }) {
     return await Category.findByIdAndUpdate(_id, { $set: data }, { omitUndefined: true });
