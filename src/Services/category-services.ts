958002d4f3b26bc1518d7f305bd33bef9e67c75c
PranoySKumar <pranoyskumar@gmail.com>
2022-11-10 10:48:00 +0530

    + delete category method

--- src/Services/category-services.ts
+++ src/Services/category-services.ts
@@ -2,6 +2,9 @@ import { Category, ICategory } from "../Models";
 
 class CategoryServices {
   static async addCategory(name: string, type: string) {
-    return await new Category({ name, type });
+    return await new Category({ name, type }).save();
+  }
+  static async deleteCategory(_id: string) {
+    return await Category.findByIdAndDelete(_id);
   }
 }
