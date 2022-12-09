d8f99a3cbaeb121188cfe8d9a1cd2205699ef37c
PranoySKumar <pranoyskumar@gmail.com>
2022-12-09 12:52:36 +0530

    removed announcements

--- src/Routes/dashboard/dashboard-announcement-routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import AnnouncementController from "../../Controllers/announcement-controller";
-
-const dashboardAnnouncementRoutes = Router();
-
-dashboardAnnouncementRoutes.post("/announcements", AnnouncementController.createAnnouncement);
-dashboardAnnouncementRoutes.get("/announcements", AnnouncementController.getAllAnnouncements);
-
-export default dashboardAnnouncementRoutes;
--- src/Routes/user/user-announcement-routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import AnnouncementController from "../../Controllers/announcement-controller";
-
-const userAnnouncementRoutes = Router();
-
-userAnnouncementRoutes.get("/announcements", AnnouncementController.getAllAnnouncements);
-
-export default userAnnouncementRoutes;
